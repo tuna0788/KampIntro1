3872d622eda297a1bec6e0196f432c5bce4d4100
Tuna Balcıoğlu <tuna0738@hotmail.com>
2021-08-18 11:20:43 +0300

    18.08.2021 5. Eğitim Dersi

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string SirketAdi { get; set; }
+        public string TcNo { get; set; }
+        public string VengiNo { get; set; }
+
+
+
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tuna Balcıoğlu
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Tuna";
+            musteri1.Soyadi = "Balcıoğlu";
+            musteri1.TcNo = "33343424324";
+            musteri1.VengiNo = "33333";
+            musteri1.SirketAdi = "?";
+
+            // Gerçek Müşteri - Tüzel Müşteri
+            // SOLID
+        }
+    }
+}
